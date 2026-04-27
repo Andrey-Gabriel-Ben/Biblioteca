4f16fb6774ec2f7caa09545de72ffeb77ea36024
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-04-26 21:07:23 -0300

    modificando o cmd para executar a main

---
+++ arquivos/ByteBook.bat
@@ -0,0 +1,13 @@
+@echo off
+:: Garante que o console use UTF-8
+chcp 65001 > nul
+
+:: TÍTULO DA JANELA
+title ByteBook - Sistema de Gerenciamento
+
+:: O comando abaixo une a biblioteca do Postgres ao seu código
+:: O "." significa "procure nesta pasta" e o ";" separa os arquivos
+java -cp ".;postgresql-42.7.10.jar" ByteBook
+
+:: Impede que o CMD feche sozinho
+pause
